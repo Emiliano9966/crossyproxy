5727b060b2ea6544174b56b4948342f9a81253db
Emiliano <76189306+Emiliano9966@users.noreply.github.com>
2025-05-03 18:00:24 -0500

    Update and rename resource.js to resource.ts

--- api/resource.ts
+++ api/resource.ts
@@ -1,16 +1,15 @@
+import axios from 'axios';
 
-const axios = require('axios');
-
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
   const url = req.query.url;
   if (!url) return res.status(400).send('Missing URL');
 
   try {
-    const response = await axios.get(url, { responseType: 'arraybuffer' });
+    const response = await axios.get(url as string, { responseType: 'arraybuffer' });
     res.setHeader('Content-Type', response.headers['content-type']);
     res.send(response.data);
   } catch (err) {
     console.error(err);
     res.status(500).send('Error loading resource');
   }
-};
+}
