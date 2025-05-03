818ca8561b9bda3c8c4da5f4fa90d27c735f71a4
Emiliano <76189306+Emiliano9966@users.noreply.github.com>
2025-05-03 18:00:06 -0500

    Update and rename proxy.js to proxy.ts

--- api/proxy.ts
+++ api/proxy.ts
@@ -1,36 +1,35 @@
+import axios from 'axios';
+import cheerio from 'cheerio';
 
-const axios = require('axios');
-const cheerio = require('cheerio');
-
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
   const url = req.query.url;
   if (!url) return res.status(400).send('Missing URL');
 
   try {
-    const response = await axios.get(url);
+    const response = await axios.get(url as string);
     const $ = cheerio.load(response.data);
 
     $('a').each((_, el) => {
       const href = $(el).attr('href');
       if (href && !href.startsWith('javascript')) {
-        const abs = new URL(href, url).href;
-        $(el).attr('href', '/proxy?url=' + encodeURIComponent(abs));
+        const abs = new URL(href, url as string).href;
+        $(el).attr('href', `/proxy?url=${encodeURIComponent(abs)}`);
       }
     });
 
     $('img, script, iframe').each((_, el) => {
       const src = $(el).attr('src');
       if (src) {
-        const abs = new URL(src, url).href;
-        $(el).attr('src', '/resource?url=' + encodeURIComponent(abs));
+        const abs = new URL(src, url as string).href;
+        $(el).attr('src', `/resource?url=${encodeURIComponent(abs)}`);
       }
     });
 
     $('link').each((_, el) => {
       const href = $(el).attr('href');
       if (href) {
-        const abs = new URL(href, url).href;
-        $(el).attr('href', '/resource?url=' + encodeURIComponent(abs));
+        const abs = new URL(href, url as string).href;
+        $(el).attr('href', `/resource?url=${encodeURIComponent(abs)}`);
       }
     });
 
@@ -40,4 +39,4 @@ module.exports = async (req, res) => {
     console.error(err);
     res.status(500).send('Error fetching page');
   }
-};
+}
