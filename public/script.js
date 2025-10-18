255adc8980576d5e4d8ebfaefcb32db0f5286b13
Emiliano <76189306+Emiliano9966@users.noreply.github.com>
2025-10-18 17:53:49 -0500

    Delete public directory

--- public/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('proxyForm').addEventListener('submit', function(e) {
-  e.preventDefault();
-  const url = document.getElementById('urlInput').value;
-  if (url) {
-    window.location.href = '/proxy?url=' + encodeURIComponent(url);
-  }
-});
